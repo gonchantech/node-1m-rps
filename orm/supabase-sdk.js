bf66ab1602bf3d7244c8e81487f09790e950f795
Gonchantech <gonchantech@gmail.com>
2026-02-08 18:05:58 +0900

    add key (.env file is included intentionally)

--- orm/supabase-sdk.js
+++ orm/supabase-sdk.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_SECRET_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
